9482658e90043a8dc10c66cba76e7a8570691346
Richard King <ravergeek@gmail.com>
2022-06-24 08:07:23 +0100

    forgot to commit

--- src/InstantaneousCostsFields.tsx
+++ src/InstantaneousCostsFields.tsx
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material";
 import { FormSpy } from "react-final-form";
 import { IThermalForm } from "./interfaces/thermal";
 import InfoThing from "./util/infoThing";
@@ -13,8 +14,15 @@ const InstantaneousCostsFields = () => {
         }}
     >
         {({ values, dirtySinceLastSubmit, submitting }) => (<div>
-            <InfoThing textA="Daily heating cost / day (Direct heating @ flat rate)" textB="(Theoretical reference only)" value={values.instantaneousHeatingCostFlatRate} preValue="£" />
-            <InfoThing textA="Daily heating cost / day (Direct heating @ peak rate)" textB="(Direct heating is usually at peak times if ToU Tariffs are used)" value={values.instantaneousHeatingCostPeakRate} preValue="£" />
+            <Grid
+                container
+                spacing={3}
+                justifyContent="left"
+                style={{ marginTop: "15px" }}
+            >
+                <InfoThing textA="Daily heating cost / day (Direct heating @ flat rate)" textB="(Theoretical reference only)" value={values.instantaneousHeatingCostFlatRate} preValue="£" />
+                <InfoThing textA="Daily heating cost / day (Direct heating @ peak rate)" textB="(Direct heating is usually at peak times if ToU Tariffs are used)" value={values.instantaneousHeatingCostPeakRate} preValue="£" />
+            </Grid>
         </div>)
         }
     </FormSpy >
