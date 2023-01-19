291e37d2e68dd544923d104a45f9e9acde5d8c62
Daniel MacRorie <danielmacrorie@Daniels-MBP.broadband>
2023-01-19 10:15:50 +0000

    added outputs to the simple parameters tab

--- src/simplifiedParameters/HouseMenu.tsx
+++ src/simplifiedParameters/HouseMenu.tsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
+import BasicContainerThing from "../util/basicContainer";
 import DropDown from "./houseTypeDropDown";
 
 const HouseMenu: React.FC = (): JSX.Element => {
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
   const [selectHouse, setSelectHouse] = useState<string>("");
   const houses = () => {
-    return ["1 bedroom flat", "2 bedroom flat", "3 bedroom flat", "4 bedroom flat", "2 bedroom house (Terraced/Semi detached)", "3 bedroom house (Terraced/Semi detached)", "4 bedroom house (Terraced/Semi detached)", "5 bedroom house (Terraced/Semi detached)"];
+    return [
+      "1 bedroom flat",
+      "2 bedroom flat",
+      "3 bedroom flat",
+      "4 bedroom flat",
+      "2 bedroom house (Terraced/Semi detached)",
+      "3 bedroom house (Terraced/Semi detached)",
+      "4 bedroom house (Terraced/Semi detached)",
+      "5 bedroom house (Terraced/Semi detached)",
+    ];
   };
 
   const toggleDropDown = () => {
     setShowDropDown(!showDropDown);
   };
-
 
   const dismissHandler = (event: React.FocusEvent<HTMLButtonElement>): void => {
     if (event.currentTarget === event.target) {
@@ -25,28 +34,25 @@ const HouseMenu: React.FC = (): JSX.Element => {
 
   return (
     <>
-      <div className="announcement">
-        <h3>
-          {"Select your house type:"}
-        </h3>
-      </div>
-      <button
-        className={showDropDown ? "active" : undefined}
-        onClick={(): void => toggleDropDown()}
-        onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
-          dismissHandler(e)
-        }
-      >
-        <div>{selectHouse ? selectHouse : "Select ..."} </div>
-        {showDropDown && (
-          <DropDown
-            houses={houses()}
-            showDropDown={false}
-            toggleDropDown={(): void => toggleDropDown()}
-            houseSelection={houseSelection}
-          />
-        )}
-      </button>
+      <BasicContainerThing title="Select your house type:">
+        <button
+          className={showDropDown ? "active" : undefined}
+          onClick={(): void => toggleDropDown()}
+          onBlur={(e: React.FocusEvent<HTMLButtonElement>): void =>
+            dismissHandler(e)
+          }
+        >
+          <div>{selectHouse ? selectHouse : "Select ..."} </div>
+          {showDropDown && (
+            <DropDown
+              houses={houses()}
+              showDropDown={false}
+              toggleDropDown={(): void => toggleDropDown()}
+              houseSelection={houseSelection}
+            />
+          )}
+        </button>
+      </BasicContainerThing>
     </>
   );
 };
